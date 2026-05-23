5a6498c360e699ddb5f0832c657b7fa3ea224a85
Alba <albalopez5110@gmail.com>
2026-05-23 19:00:28 +0200

    Corrected the z for IK and the theta and estimated d_cable for a coordinate movement

--- ros2_ws/src/cable_robot_kinematics/src/ik_bridge.cpp
+++ ros2_ws/src/cable_robot_kinematics/src/ik_bridge.cpp
@@ -14,8 +14,9 @@ using namespace std::chrono_literals;
 class IKBridge : public rclcpp::Node
 {
     public:
-        IKBridge() : Node("ik_bridge"), n_segment_(4), d_cable_(0.02), phi_cable_1_(M_PI),
-        phi_cable_2_(M_PI / 2.0), phi_cable_3_(-M_PI / 2.0)
+        IKBridge() : Node("ik_bridge"), n_segment_(4), d_cable_(0.11),
+        segment_spacing_(0.05675),
+        phi_cable_1_(M_PI), phi_cable_2_(M_PI / 2.0), phi_cable_3_(-M_PI / 2.0)
         {
             cable_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("cable_joint_commands", 10);
             bend_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
@@ -31,12 +32,13 @@ class IKBridge : public rclcpp::Node
             double z = msg->pose.position.z;
 
             
-            auto [bx_unused, by_unused, theta, phi] = cable_robot_kinematics::per_segment_bending(x, y, z, n_segment_);
+            double L_total = n_segment_ * segment_spacing_;
+            double z_arc = z + L_total;
+
+            auto [bx_unused, by_unused, theta, phi] = cable_robot_kinematics::per_segment_bending(x, y, z_arc, n_segment_);
             (void)bx_unused; (void)by_unused;
 
-            double theta_total = theta * n_segment_;
-
-            auto [c1, c2, c3] = cable_robot_kinematics::get_cable_pull(theta_total, phi, d_cable_, phi_cable_1_,
+            auto [c1, c2, c3] = cable_robot_kinematics::get_cable_pull(theta, phi, d_cable_, phi_cable_1_,
                 phi_cable_2_, phi_cable_3_);
 
             auto stamp = this->now();
@@ -54,6 +56,7 @@ class IKBridge : public rclcpp::Node
 
         int n_segment_;
         double d_cable_;
+        double segment_spacing_;
         double phi_cable_1_;
         double phi_cable_2_;
         double phi_cable_3_;
